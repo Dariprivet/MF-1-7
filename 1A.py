060d83a37cbde238b35c29a292cc869451457284
Dariprivet <167632171+Dariprivet@users.noreply.github.com>
2024-04-23 18:35:11 +0300

    Create 1A.py

---
+++ 1A.py
@@ -0,0 +1,18 @@
+from math import*
+x=2
+z=3
+r1=z**3*x+3*(x**z)-0.3
+print('r1',r1)
+
+x=1
+r2=sqrt(log(abs((sin(x)**3)))+1)-exp(-x)
+print('r2',r2)
+
+x=2
+y=2
+r3=(0.3*cos(x**2)**2+1)/(2*x*y)+6
+print('r3',r3)
+
+x=3
+r4=atan(2*x+7)/(x+4.2)+3**sqrt(x)
+print('r4',r4)
