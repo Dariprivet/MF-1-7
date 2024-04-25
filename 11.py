3c40cc4e72b2afcc74c84820a71ecb28c3ebab14
Dariprivet <167632171+Dariprivet@users.noreply.github.com>
2024-04-25 23:57:48 +0300

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,17 +1,14 @@
 def f(x):
     return -260+121*x-12*x**2+x**3
-
 def integrate_trapezoidal(a, b, n):
-    h = (b-a) / n  # Вычисляем шаг разбиения
-    result = 0.5*(f(a)+f(b))  # Используем формулу трапеций для первого и последнего значения
-    for i in range(1,n):  # Суммируем значения функции внутри интервала
+    h = (b-a) / n  
+    result = 0.5*(f(a)+f(b))  
+    for i in range(1,n): 
         result += f(a+i*h)
-    result *= h  # Умножаем сумму на шаг разбиения
+    result *= h  
     return result
-
-a = 0  # Нижний предел интегрирования
-b = 3  # Верхний предел интегрирования
-n = 4  # Количество интервалов разбиения
-
+a = 0  
+b = 3  
+n = 4  
 integral = integrate_trapezoidal(a,b,n)
 print("Значение интеграла:", integral)
