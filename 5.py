b3bd97f7b1e20b85972545b9196cf6bb1214c463
Dariprivet <167632171+Dariprivet@users.noreply.github.com>
2024-05-19 15:32:00 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,15 +1,21 @@
-def uk(x, k):
-    return (-1)**(k+1)*(x**(k+1)/k*(k+1))
-def calculate_sum(x, n):
-    total_sum=0
-    for k in range(1,n+1):
-        total_sum +=uk(x,k)
-    return total_sum
-x_values = [0.1,0.3,0.4,0.7,1.0]
-n = 10
-print("|   x   |    S    |")
-print("-------------------")
-for x in x_values:
-    result = calculate_sum(x, n)
-    print(f"| {x}  |  {result:.6f} |")
-print("-------------------")
+N = int(input("Введите N: "))
+X = []
+
+for i in range(0,5):
+    x = float(input("Введите x: "))
+    # print("type_x:", type(x))
+    X.append(x)
+
+#X=[0.1,0.3, 0.4, 0.7, 1]
+print(' -------------------------')
+print(' | x         |         s |')
+print(' -------------------------')
+
+for x in X:
+    s = 0
+    u = -x
+    for k in range(1,N+1):
+        s += u/k
+    print(" | %8.4f | %8.4f |" % (x,s))
+
+print(' -------------------------')
