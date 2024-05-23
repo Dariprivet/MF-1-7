75216ae3b709e75e2a9542f85420fc16abce8892
Dariprivet <167632171+Dariprivet@users.noreply.github.com>
2024-05-23 19:42:59 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,23 +1,14 @@
-import random
-spisok = random.sample(range(13), 12) # 12 случайных чисел от 0 до 12
-if 0 in spisok and spisok[0] == 0:
-    product1 = 0
-    for number in spisok[:spisok.index(0)]:
-        product1 *= number
-    print("Массив:", spisok)
-    print("Произведение:", product1)
-    print("Сумма:", sum(spisok[spisok.index(0):]))
-elif 0 in spisok:
-    product1 = 1
-    for number in spisok[:spisok.index(0)]:
-        product1 *= number
-    print("Массив:", spisok)
-    print("Произведение:", product1)
-    print("Сумма:", sum(spisok[spisok.index(0):]))
-else:
-    product2 = 1
-    for number in spisok:
-        product2 *= number
+A = [x for x in range(1, 12)] #элемент слева - то, что кладем в массив.Справа - цикл перебора значений от 1 до 12
+#me = [] 
+#for x in range(1, 16):
+#    me.append(x)
+b = []
+for XI in range(len(A)):
+    if XI % 2 == 0:
+        b.append(A[XI]) # типа 2 соответствует индекс 1, поэтому ее мы добавляем в список 
+    else:
+        b.append(0)
+print(A, b, sum(b))  
     print("Массив:", spisok)
     print("Произведение всех чисел:", product2)
     print("Сумма всех чисел:", sum(spisok))
