f2316de77972681d5898474c142a807bef6b409b
Dariprivet <167632171+Dariprivet@users.noreply.github.com>
2024-05-23 19:41:07 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,12 +1,21 @@
 import numpy as np
-A = np.array([[1, 2, 3], [1, 2, 1], [3, 2, 0]]) #создание массива
-B = np.array([[4, 1, 2], [0, 4, 3], [1, 1, 1]]) #создание массива
-P = np.array([[0.1], [1.7], [-1.5]]) #создание массива
-Q = np.array([[-1.6], [0.8], [-1.5]]) #создание массива
-R = np.array([[-0.7], [1.3], [0.2]]) #создание массива
-AP = np.dot(A,Q)
-print ('AP =', AP)
-BR = np.dot(B,R)
-print ('BR =', BR)
-s = np.dot(AP.flatten(),BR.flatten())
+A=np.genfromtxt('Text\А.txt')
+print('Исходный массив A')
+print(A)
+B=np.genfromtxt('Text\В.txt')
+print('Исходный массив B')
+print(B)
+Q=np.genfromtxt('Text\Q.txt')
+print('Исходный массив Q')
+print(Q)
+R=np.genfromtxt('Text\R.txt')
+print('Исходный массив R')
+print(R)
+x = np.dot(B,Q)
+print ('x =', x)
+y = np.dot(A,R)
+print ('y =', y)
+z = x - y
+print ('z =', z)
+s = np.dot(z,y)
 print ('s =', s)
